bepzi/naepen

commit c368ddd1761158b4711343c8c333bf05f1cb808a

Add a second oscillator

Source/PluginProcessor.cpp
@@ -1,7 +1,7 @@
 #include "PluginProcessor.h"
 
-#include "processors/OscillatorAudioProcessor.h"
 #include "gui/PluginEditor.h"
+#include "processors/OscillatorAudioProcessor.h"
 
 #include "DatabaseIdentifiers.h"
 
@@ -156,19 +156,22 @@ void NaepenAudioProcessor::initialize_graph()
         getMainBusNumInputChannels(), getMainBusNumOutputChannels(), getSampleRate(),
         getBlockSize());
 
+    osc_two_node = processor_graph.addNode(std::make_unique<OscillatorAudioProcessor>(state));
+    osc_two_node->getProcessor()->setPlayConfigDetails(
+        getMainBusNumInputChannels(), getMainBusNumOutputChannels(), getSampleRate(),
+        getBlockSize());
+
     for (int channel = 0; channel < getMainBusNumOutputChannels(); ++channel) {
-        if (!processor_graph.addConnection(
-                {{osc_one_node->nodeID, channel}, {audio_output_node->nodeID, channel}})) {
-            std::fprintf(
-                stderr, "Failed to connect osc1 node to audio output node for channel %d\n",
-                channel);
-        }
+        jassert(processor_graph.addConnection(
+            {{osc_one_node->nodeID, channel}, {audio_output_node->nodeID, channel}}));
+        jassert(processor_graph.addConnection(
+            {{osc_two_node->nodeID, channel}, {audio_output_node->nodeID, channel}}));
     }
 
     // NOTE: We don't actually have to wire up the MIDI input node to the osc_one_component node,
-    // probably because osc_one_component AudioProcessor _doesn't_ accept MIDI and we handle retrieving
-    // messages, the virtual keyboard, etc. in this AudioProcessor before passing
-    // it on to the graph to handle. I think.
+    // probably because osc_one_component AudioProcessor _doesn't_ accept MIDI and we handle
+    // retrieving messages, the virtual keyboard, etc. in this AudioProcessor before passing it on
+    // to the graph to handle. I think.
 }
 
 // TODO: Add static methods to each automatable component to generate their own parameters
@@ -225,7 +228,52 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
             std::move(osc_one_filter_q));
     }
 
-    return {std::move(master_gain_param), std::move(osc_one_group)};
+    // Parameters for Oscillator 2
+    // ====================================================
+    auto osc_two_group = std::make_unique<AudioProcessorParameterGroup>(
+        DatabaseIdentifiers::OSC_TWO_GROUP.toString(), "Oscillator 2", "|");
+    {
+        NormalisableRange<float> adr_range = {0.0f, 5.0f, 0.001f};
+        adr_range.setSkewForCentre(0.75f);
+
+        NormalisableRange<float> sustain_range = {0.0f, 1.0f, 0.01f};
+
+        auto osc_two_gain_attack = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK.toString(), "Osc 2 Gain Attack", adr_range,
+            0.05f, "s");
+        auto osc_two_gain_decay = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_GAIN_DECAY.toString(), "Osc 2 Gain Decay", adr_range,
+            0.25f, "s");
+        auto osc_two_gain_sustain = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_GAIN_SUSTAIN.toString(), "Osc 2 Gain Sustain",
+            sustain_range, 1.0f);
+        auto osc_two_gain_release = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_GAIN_RELEASE.toString(), "Osc 2 Gain Release", adr_range,
+            0.15f, "s");
+
+        osc_two_group->addChild(
+            std::move(osc_two_gain_attack), std::move(osc_two_gain_decay),
+            std::move(osc_two_gain_sustain), std::move(osc_two_gain_release));
+
+        auto osc_two_filter_enabled = std::make_unique<AudioParameterBool>(
+            DatabaseIdentifiers::OSC_TWO_FILTER_ENABLED.toString(), "Osc 2 Filter Enabled", false);
+
+        NormalisableRange<float> cutoff_range = {1.0f, 10000.0f, 0.1f};
+        cutoff_range.setSkewForCentre(700.0f);
+        auto osc_two_filter_cutoff = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_FILTER_CUTOFF.toString(), "Osc 2 Filter Cutoff",
+            cutoff_range, 5000.0f, "Hz");
+        NormalisableRange<float> q_range = {0.5f, 20.0f, 0.01f};
+        q_range.setSkewForCentre(5.0f);
+        auto osc_two_filter_q = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_FILTER_Q.toString(), "Osc 2 Filter Q", q_range, 0.5f);
+
+        osc_two_group->addChild(
+            std::move(osc_two_filter_enabled), std::move(osc_two_filter_cutoff),
+            std::move(osc_two_filter_q));
+    }
+
+    return {std::move(master_gain_param), std::move(osc_one_group), std::move(osc_two_group)};
 }
 
 //==============================================================================

Source/DatabaseIdentifiers.cpp
@@ -16,4 +16,14 @@ const Identifier DatabaseIdentifiers::OSC_ONE_FILTER_ENABLED {"OscOneFilterEnabl
 const Identifier DatabaseIdentifiers::OSC_ONE_FILTER_CUTOFF {"OscOneFilterCutoff"};
 const Identifier DatabaseIdentifiers::OSC_ONE_FILTER_Q {"OscOneFilterQ"};
 
+// Parameters for Oscillator 1
 // ====================================================
+const Identifier DatabaseIdentifiers::OSC_TWO_GROUP {"OscTwoGroup"};
+const Identifier DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK {"OscTwoGainAttack"};
+const Identifier DatabaseIdentifiers::OSC_TWO_GAIN_DECAY {"OscTwoGainDecay"};
+const Identifier DatabaseIdentifiers::OSC_TWO_GAIN_SUSTAIN {"OscTwoGainSustain"};
+const Identifier DatabaseIdentifiers::OSC_TWO_GAIN_RELEASE {"OscTwoGainRelease"};
+
+const Identifier DatabaseIdentifiers::OSC_TWO_FILTER_ENABLED {"OscTwoFilterEnabled"};
+const Identifier DatabaseIdentifiers::OSC_TWO_FILTER_CUTOFF {"OscTwoFilterCutoff"};
+const Identifier DatabaseIdentifiers::OSC_TWO_FILTER_Q {"OscTwoFilterQ"};

Source/DatabaseIdentifiers.h
@@ -19,5 +19,15 @@ struct DatabaseIdentifiers {
     static const Identifier OSC_ONE_FILTER_CUTOFF;
     static const Identifier OSC_ONE_FILTER_Q;
 
+    // Parameters for Oscillator 2
     // ====================================================
+    static const Identifier OSC_TWO_GROUP;
+    static const Identifier OSC_TWO_GAIN_ATTACK;
+    static const Identifier OSC_TWO_GAIN_DECAY;
+    static const Identifier OSC_TWO_GAIN_SUSTAIN;
+    static const Identifier OSC_TWO_GAIN_RELEASE;
+
+    static const Identifier OSC_TWO_FILTER_ENABLED;
+    static const Identifier OSC_TWO_FILTER_CUTOFF;
+    static const Identifier OSC_TWO_FILTER_Q;
 };

Source/PluginProcessor.h
@@ -60,7 +60,9 @@ private:
     void initialize_graph();
     [[maybe_unused]] Node::Ptr midi_input_node;
     Node::Ptr audio_output_node;
+
     Node::Ptr osc_one_node;
+    Node::Ptr osc_two_node;
 
     MidiMessageCollector midi_collector;
 

Source/dsp/BandlimitedOscillator.cpp
@@ -251,33 +251,3 @@ BandlimitedOscillator::LookupTable make_engineers_sawtooth(double top_freq, doub
 
     return BandlimitedOscillator::from_full_bandwidth(samples);
 }
-
-BandlimitedOscillator::LookupTable make_musicians_sawtooth(double top_freq, double sample_rate)
-{
-    assert(top_freq > 0.0);
-    assert(sample_rate > 0.0);
-
-    std::array<double, BandlimitedOscillator::T> samples = {};
-
-    double phase = 0.0;
-    double phase_delta = (2 * M_PI) / (double)(BandlimitedOscillator::T);
-    auto num_harmonics = (size_t)(sample_rate / (2.0 * top_freq));
-
-    for (auto &f : samples) {
-        for (size_t i = 1; i <= num_harmonics; ++i) {
-            float amp = 1.0f / i;
-            if (i > (size_t)(num_harmonics / 1.4)) {
-                amp = 0.75f / i;
-            }
-
-            f += std::sin(phase * i) * amp;
-        }
-
-        // Sine wave summation makes an "inverted" sawtooth, so flip it back over
-        f = -f;
-
-        phase += phase_delta;
-    }
-
-    return BandlimitedOscillator::from_full_bandwidth(samples);
-}

Source/gui/PluginEditor.cpp
@@ -19,6 +19,14 @@ NaepenAudioProcessorEditor::NaepenAudioProcessorEditor(NaepenAudioProcessor &pro
         DatabaseIdentifiers::OSC_ONE_FILTER_ENABLED.toString(),
         DatabaseIdentifiers::OSC_ONE_FILTER_CUTOFF.toString(),
         DatabaseIdentifiers::OSC_ONE_FILTER_Q.toString()),
+    osc_two_component(
+        naepenProcessor.state, DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK.toString(),
+        DatabaseIdentifiers::OSC_TWO_GAIN_DECAY.toString(),
+        DatabaseIdentifiers::OSC_TWO_GAIN_SUSTAIN.toString(),
+        DatabaseIdentifiers::OSC_TWO_GAIN_RELEASE.toString(),
+        DatabaseIdentifiers::OSC_TWO_FILTER_ENABLED.toString(),
+        DatabaseIdentifiers::OSC_TWO_FILTER_CUTOFF.toString(),
+        DatabaseIdentifiers::OSC_TWO_FILTER_Q.toString()),
     midi_keyboard(naepenProcessor.virtual_keyboard_state, MidiKeyboardComponent::horizontalKeyboard)
 {
     midi_keyboard.setOctaveForMiddleC(4);
@@ -26,7 +34,9 @@ NaepenAudioProcessorEditor::NaepenAudioProcessorEditor(NaepenAudioProcessor &pro
     addAndMakeVisible(master_gain_slider);
 
     addAndMakeVisible(midi_keyboard);
+
     addAndMakeVisible(osc_one_component);
+    addAndMakeVisible(osc_two_component);
 
     setSize(EDITOR_WIDTH, EDITOR_HEIGHT);
 }
@@ -48,4 +58,5 @@ void NaepenAudioProcessorEditor::resized()
     area.removeFromLeft(128);
 
     osc_one_component.setBounds(area.removeFromTop(area.getHeight() / 2));
+    osc_two_component.setBounds(area);
 }

Source/gui/PluginEditor.h
@@ -19,6 +19,7 @@ private:
     AudioProcessorValueTreeState::SliderAttachment master_gain_slider_attachment;
 
     OscillatorComponent osc_one_component;
+    OscillatorComponent osc_two_component;
 
     MidiKeyboardComponent midi_keyboard;
 

Source/processors/OscillatorAudioProcessor.h
@@ -30,5 +30,4 @@ private:
     std::shared_ptr<const BandlimitedOscillator::LookupTable> triangle_table;
     std::shared_ptr<const BandlimitedOscillator::LookupTable> square_table;
     std::shared_ptr<const BandlimitedOscillator::LookupTable> engineers_sawtooth_table;
-    std::shared_ptr<const BandlimitedOscillator::LookupTable> musicians_sawtooth_table;
 };

commit dfc1be7a3907dd35fa5ed104271f6cd6b0f5ca36

Don't wrap important stateful calls in jassert()
`jassert()` gets compiled away when compiling in release mode, so
these calls were being stripped out and the processor graph wasn't
rendering audio.

This bug was introduced in c368ddd

Source/PluginProcessor.cpp
@@ -178,10 +178,11 @@ void NaepenAudioProcessor::initialize_graph()
         getBlockSize());
 
     for (int channel = 0; channel < getMainBusNumOutputChannels(); ++channel) {
-        jassert(processor_graph.addConnection(
-            {{osc_one_node->nodeID, channel}, {audio_output_node->nodeID, channel}}));
-        jassert(processor_graph.addConnection(
-            {{osc_two_node->nodeID, channel}, {audio_output_node->nodeID, channel}}));
+        // TODO: Be more robust against failure to connect graph nodes
+        processor_graph.addConnection(
+            {{osc_one_node->nodeID, channel}, {audio_output_node->nodeID, channel}});
+        processor_graph.addConnection(
+            {{osc_two_node->nodeID, channel}, {audio_output_node->nodeID, channel}});
     }
 
     // NOTE: We don't actually have to wire up the MIDI input node to the osc_one_component node,