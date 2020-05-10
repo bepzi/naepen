bepzi/naepen

commit c1182e9ffb586fa2eddb9d389c50aaa66b7894b0

Add pan controls

Source/gui/OscillatorComponent.cpp
@@ -5,12 +5,14 @@
 #include <JuceHeader.h>
 
 OscillatorComponent::OscillatorComponent(
-    APVTS &state, const Identifier &waveform_id, const String &gain_attack_id,
+    APVTS &state, const Identifier &waveform_id, const String &pan_id, const String &gain_attack_id,
     const String &gain_decay_id, const String &gain_sustain_id, const String &gain_release_id,
     const String &filter_type_id, const String &filter_enabled_id, const String &filter_cutoff_id,
     const String &filter_q_id) :
     waveform_selector_model(std::make_unique<WaveformSelectorListBoxModel>(state, waveform_id)),
     waveform_selector("", waveform_selector_model.get()),
+
+    pan_slider_attachment(state, pan_id, pan_slider),
 
     gain_attack_slider_attachment(state, gain_attack_id, gain_attack_slider),
     gain_decay_slider_attachment(state, gain_decay_id, gain_decay_slider),
@@ -25,6 +27,8 @@ OscillatorComponent::OscillatorComponent(
     waveform_selector.updateContent();
     waveform_selector.selectRow(0);
     addAndMakeVisible(waveform_selector);
+
+    addAndMakeVisible(pan_slider);
 
     addAndMakeVisible(gain_attack_slider);
     addAndMakeVisible(gain_decay_slider);
@@ -79,7 +83,9 @@ void OscillatorComponent::resized()
     auto waveform_controls = area.reduced(0, 32);
 
     waveform_selector.setBounds(waveform_controls.removeFromLeft(col_width * 2)
-                                    .removeFromTop(waveform_controls.getHeight() / 2));
+                                    .reduced(0, waveform_controls.getHeight() / 2));
+
+    pan_slider.setBounds(waveform_controls.removeFromLeft(col_width));
 
     gain_attack_slider.setBounds(gain_controls.removeFromLeft(col_width));
     gain_decay_slider.setBounds(gain_controls.removeFromLeft(col_width));

Source/DatabaseIdentifiers.cpp
@@ -10,6 +10,8 @@ const Identifier DatabaseIdentifiers::OSC_ONE_GROUP {"OscOneGroup"};
 
 const Identifier DatabaseIdentifiers::OSC_ONE_WAVEFORM {"OscOneWaveform"};
 
+const Identifier DatabaseIdentifiers::OSC_ONE_PAN {"OscOnePan"};
+
 const Identifier DatabaseIdentifiers::OSC_ONE_GAIN {"OscOneGain"};
 const Identifier DatabaseIdentifiers::OSC_ONE_GAIN_ATTACK {"OscOneGainAttack"};
 const Identifier DatabaseIdentifiers::OSC_ONE_GAIN_DECAY {"OscOneGainDecay"};
@@ -26,6 +28,8 @@ const Identifier DatabaseIdentifiers::OSC_ONE_FILTER_Q {"OscOneFilterQ"};
 const Identifier DatabaseIdentifiers::OSC_TWO_GROUP {"OscTwoGroup"};
 
 const Identifier DatabaseIdentifiers::OSC_TWO_WAVEFORM {"OscTwoWaveform"};
+
+const Identifier DatabaseIdentifiers::OSC_TWO_PAN {"OscTwoPan"};
 
 const Identifier DatabaseIdentifiers::OSC_TWO_GAIN {"OscTwoGain"};
 const Identifier DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK {"OscTwoGainAttack"};

Source/DatabaseIdentifiers.h
@@ -13,6 +13,8 @@ struct DatabaseIdentifiers {
 
     static const Identifier OSC_ONE_WAVEFORM;
 
+    static const Identifier OSC_ONE_PAN;
+
     static const Identifier OSC_ONE_GAIN;
     static const Identifier OSC_ONE_GAIN_ATTACK;
     static const Identifier OSC_ONE_GAIN_DECAY;
@@ -29,6 +31,8 @@ struct DatabaseIdentifiers {
     static const Identifier OSC_TWO_GROUP;
 
     static const Identifier OSC_TWO_WAVEFORM;
+
+    static const Identifier OSC_TWO_PAN;
 
     static const Identifier OSC_TWO_GAIN;
     static const Identifier OSC_TWO_GAIN_ATTACK;

Source/PluginProcessor.cpp
@@ -150,7 +150,8 @@ void NaepenAudioProcessor::initialize_graph()
             AudioProcessorGraph::AudioGraphIOProcessor::midiInputNode));
 
     osc_one_node = processor_graph.addNode(std::make_unique<OscillatorAudioProcessor>(
-        state, DatabaseIdentifiers::OSC_ONE_WAVEFORM, DatabaseIdentifiers::OSC_ONE_GAIN.toString(),
+        state, DatabaseIdentifiers::OSC_ONE_WAVEFORM, DatabaseIdentifiers::OSC_ONE_PAN.toString(),
+        DatabaseIdentifiers::OSC_ONE_GAIN.toString(),
         DatabaseIdentifiers::OSC_ONE_GAIN_ATTACK.toString(),
         DatabaseIdentifiers::OSC_ONE_GAIN_DECAY.toString(),
         DatabaseIdentifiers::OSC_ONE_GAIN_SUSTAIN.toString(),
@@ -164,7 +165,8 @@ void NaepenAudioProcessor::initialize_graph()
         getBlockSize());
 
     osc_two_node = processor_graph.addNode(std::make_unique<OscillatorAudioProcessor>(
-        state, DatabaseIdentifiers::OSC_TWO_WAVEFORM, DatabaseIdentifiers::OSC_TWO_GAIN.toString(),
+        state, DatabaseIdentifiers::OSC_TWO_WAVEFORM, DatabaseIdentifiers::OSC_TWO_PAN.toString(),
+        DatabaseIdentifiers::OSC_TWO_GAIN.toString(),
         DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK.toString(),
         DatabaseIdentifiers::OSC_TWO_GAIN_DECAY.toString(),
         DatabaseIdentifiers::OSC_TWO_GAIN_SUSTAIN.toString(),
@@ -196,11 +198,13 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
 {
     // Global parameters
     // ====================================================
-    NormalisableRange<float> master_gain_range = {0.0f, 1.0f, 0.001f};
+    NormalisableRange<float> gain_range = {0.0f, 1.0f, 0.001f};
     auto master_gain_param = std::make_unique<AudioParameterFloat>(
-        DatabaseIdentifiers::MASTER_GAIN.toString(), "Master Gain", master_gain_range, 0.5f);
+        DatabaseIdentifiers::MASTER_GAIN.toString(), "Master Gain", gain_range, 0.5f);
 
     const StringArray filter_type_choices = {"Lowpass", "Highpass", "Bandpass"};
+
+    NormalisableRange<float> pan_range = {0.0f, 1.0f, 0.001f};
 
     // Parameters for Oscillator 1
     // ====================================================
@@ -212,8 +216,11 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
 
         NormalisableRange<float> sustain_range = {0.0f, 1.0f, 0.01f};
 
+        auto osc_one_pan = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_ONE_PAN.toString(), "Osc 1 Pan", pan_range, 0.5f);
+
         auto osc_one_gain = std::make_unique<AudioParameterFloat>(
-            DatabaseIdentifiers::OSC_ONE_GAIN.toString(), "Osc 1 Gain", master_gain_range, 0.5f);
+            DatabaseIdentifiers::OSC_ONE_GAIN.toString(), "Osc 1 Gain", gain_range, 0.5f);
         auto osc_one_gain_attack = std::make_unique<AudioParameterFloat>(
             DatabaseIdentifiers::OSC_ONE_GAIN_ATTACK.toString(), "Osc 1 Gain Attack", adr_range,
             0.05f, "s");
@@ -228,8 +235,9 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
             0.15f, "s");
 
         osc_one_group->addChild(
-            std::move(osc_one_gain), std::move(osc_one_gain_attack), std::move(osc_one_gain_decay),
-            std::move(osc_one_gain_sustain), std::move(osc_one_gain_release));
+            std::move(osc_one_pan), std::move(osc_one_gain), std::move(osc_one_gain_attack),
+            std::move(osc_one_gain_decay), std::move(osc_one_gain_sustain),
+            std::move(osc_one_gain_release));
 
         auto osc_one_filter_type = std::make_unique<AudioParameterChoice>(
             DatabaseIdentifiers::OSC_ONE_FILTER_TYPE.toString(), "Osc 1 Filter Type",
@@ -262,8 +270,11 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
 
         NormalisableRange<float> sustain_range = {0.0f, 1.0f, 0.01f};
 
+        auto osc_two_pan = std::make_unique<AudioParameterFloat>(
+            DatabaseIdentifiers::OSC_TWO_PAN.toString(), "Osc 2 Pan", pan_range, 0.5f);
+
         auto osc_two_gain = std::make_unique<AudioParameterFloat>(
-            DatabaseIdentifiers::OSC_TWO_GAIN.toString(), "Osc 2 Gain", master_gain_range, 0.0f);
+            DatabaseIdentifiers::OSC_TWO_GAIN.toString(), "Osc 2 Gain", gain_range, 0.0f);
         auto osc_two_gain_attack = std::make_unique<AudioParameterFloat>(
             DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK.toString(), "Osc 2 Gain Attack", adr_range,
             0.05f, "s");
@@ -278,8 +289,9 @@ APVTS::ParameterLayout NaepenAudioProcessor::create_parameter_layout()
             0.15f, "s");
 
         osc_two_group->addChild(
-            std::move(osc_two_gain), std::move(osc_two_gain_attack), std::move(osc_two_gain_decay),
-            std::move(osc_two_gain_sustain), std::move(osc_two_gain_release));
+            std::move(osc_two_pan), std::move(osc_two_gain), std::move(osc_two_gain_attack),
+            std::move(osc_two_gain_decay), std::move(osc_two_gain_sustain),
+            std::move(osc_two_gain_release));
 
         auto osc_two_filter_type = std::make_unique<AudioParameterChoice>(
             DatabaseIdentifiers::OSC_TWO_FILTER_TYPE.toString(), "Osc 2 Filter Type",

Source/dsp/OscillatorVoice.cpp
@@ -20,12 +20,14 @@ bool OscillatorSound::appliesToChannel(int midi_channel)
 
 OscillatorVoice::OscillatorVoice(
     std::unique_ptr<Oscillator> oscillator, AudioProcessorValueTreeState &apvts,
-    std::atomic<float> *gain_attack, std::atomic<float> *gain_decay,
+    std::atomic<float> *pan, std::atomic<float> *gain_attack, std::atomic<float> *gain_decay,
     std::atomic<float> *gain_sustain, std::atomic<float> *gain_release, Identifier filter_type_id,
     std::atomic<float> *filter_enabled, std::atomic<float> *filter_cutoff,
     std::atomic<float> *filter_q) :
     osc(std::move(oscillator)),
     state(apvts),
+
+    pan(pan),
 
     gain_attack(gain_attack),
     gain_decay(gain_decay),
@@ -79,6 +81,8 @@ void OscillatorVoice::controllerMoved(int, int) {}
 void OscillatorVoice::renderNextBlock(
     AudioBuffer<float> &output_buffer, int start_sample, int num_samples)
 {
+    jassert(output_buffer.getNumChannels() == 2);
+
     auto filter_type = static_cast<SvfFilter::Type>(
         reinterpret_cast<AudioParameterChoice *>(state.getParameter(filter_type_id))->getIndex());
     if (filter_type != filter.get_type()) {
@@ -100,9 +104,9 @@ void OscillatorVoice::renderNextBlock(
             sample = filter.get_next_sample(sample);
         }
 
-        for (int channel = 0; channel < output_buffer.getNumChannels(); ++channel) {
-            output_buffer.addSample(channel, i, sample);
-        }
+        output_buffer.addSample(
+            0, i, sample * sinf((1 - *pan) * MathConstants<float>::halfPi));  // Left
+        output_buffer.addSample(1, i, sample * sinf(*pan * MathConstants<float>::halfPi));  // Right
     }
 }
 

Source/dsp/OscillatorVoice.h
@@ -15,7 +15,7 @@ class OscillatorVoice : public SynthesiserVoice {
 public:
     OscillatorVoice(
         std::unique_ptr<Oscillator> oscillator, AudioProcessorValueTreeState &apvts,
-        std::atomic<float> *gain_attack, std::atomic<float> *gain_decay,
+        std::atomic<float> *pan, std::atomic<float> *gain_attack, std::atomic<float> *gain_decay,
         std::atomic<float> *gain_sustain, std::atomic<float> *gain_release,
         Identifier filter_type_id, std::atomic<float> *filter_enabled,
         std::atomic<float> *filter_cutoff, std::atomic<float> *filter_q);
@@ -47,6 +47,8 @@ private:
     AudioProcessorValueTreeState &state;
 
     float level = 0.0f;
+
+    std::atomic<float> *pan;
 
     ADSR gain_envelope;
     std::atomic<float> *gain_attack;

Source/gui/OscillatorComponent.h
@@ -7,7 +7,7 @@
 class OscillatorComponent : public Component {
 public:
     OscillatorComponent(
-        AudioProcessorValueTreeState &state, const Identifier &waveform_id,
+        AudioProcessorValueTreeState &state, const Identifier &waveform_id, const String &pan_id,
         const String &gain_attack_id, const String &gain_decay_id, const String &gain_sustain_id,
         const String &gain_release_id, const String &filter_type_id,
         const String &filter_enabled_id, const String &filter_cutoff_id, const String &filter_q_id);
@@ -21,6 +21,9 @@ private:
     // TODO: Perhaps this should be done with a TreeView?
     std::unique_ptr<WaveformSelectorListBoxModel> waveform_selector_model;
     ListBox waveform_selector;
+
+    Slider pan_slider {Slider::RotaryHorizontalVerticalDrag, Slider::TextBoxBelow};
+    APVTS::SliderAttachment pan_slider_attachment;
 
     Slider gain_attack_slider {Slider::RotaryHorizontalVerticalDrag, Slider::TextBoxBelow};
     Slider gain_decay_slider {Slider::RotaryHorizontalVerticalDrag, Slider::TextBoxBelow};

Source/gui/PluginEditor.cpp
@@ -15,6 +15,8 @@ NaepenAudioProcessorEditor::NaepenAudioProcessorEditor(NaepenAudioProcessor &pro
 
         DatabaseIdentifiers::OSC_ONE_WAVEFORM,
 
+        DatabaseIdentifiers::OSC_ONE_PAN.toString(),
+
         DatabaseIdentifiers::OSC_ONE_GAIN_ATTACK.toString(),
         DatabaseIdentifiers::OSC_ONE_GAIN_DECAY.toString(),
         DatabaseIdentifiers::OSC_ONE_GAIN_SUSTAIN.toString(),
@@ -29,6 +31,8 @@ NaepenAudioProcessorEditor::NaepenAudioProcessorEditor(NaepenAudioProcessor &pro
         naepenProcessor.state,
 
         DatabaseIdentifiers::OSC_TWO_WAVEFORM,
+
+        DatabaseIdentifiers::OSC_TWO_PAN.toString(),
 
         DatabaseIdentifiers::OSC_TWO_GAIN_ATTACK.toString(),
         DatabaseIdentifiers::OSC_TWO_GAIN_DECAY.toString(),

commit cb3ad5c799fcd770c6d48da634e1243f708406d5

Make waveform selector visible again
Bug introduced in c1182e9

Source/gui/OscillatorComponent.cpp
@@ -90,7 +90,7 @@ void OscillatorComponent::resized()
     auto waveform_controls = area.reduced(0, 8);
 
     waveform_selector.setBounds(waveform_controls.removeFromLeft(col_width * 2)
-                                    .reduced(0, waveform_controls.getHeight() / 2));
+                                    .reduced(0, waveform_controls.getHeight() / 4));
 
     auto detune_controls = waveform_controls.removeFromLeft(col_width);
     detune_semitones_slider.setBounds(